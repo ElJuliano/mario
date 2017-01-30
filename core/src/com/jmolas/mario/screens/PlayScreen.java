34325611f1f2512adbb54ef911b087a9a4ffc8fb
Julien <molasjulien@gmail.com>
2017-01-30 23:48:25 +0100

    -- Step 3 integration -- FitView port

--- core/src/com/jmolas/mario/screens/PlayScreen.java
+++ core/src/com/jmolas/mario/screens/PlayScreen.java
@@ -4,7 +4,12 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.jmolas.mario.MarioBros;
 
 import java.io.File;
@@ -18,10 +23,15 @@ public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
 
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+
 
     public PlayScreen(MarioBros game){
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gameCam);
     }
 
 
@@ -36,6 +46,9 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //Setting the display to the camera view
+        game.batch.setProjectionMatrix(gameCam.combined);
+
         //drawing the game
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
@@ -44,7 +57,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
