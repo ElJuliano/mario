ae8b1f8468408c4b037843a9a6cc32e420a152e2
Julien <molasjulien@gmail.com>
2017-01-30 23:27:19 +0100

    2 first steps in mario

---
+++ core/src/com/jmolas/mario/screens/PlayScreen.java
@@ -0,0 +1,69 @@
+package com.jmolas.mario.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.jmolas.mario.MarioBros;
+
+import java.io.File;
+
+/**
+ * Created by Julien on 30/01/2017.
+ */
+
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    Texture texture;
+
+
+    public PlayScreen(MarioBros game){
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        //clearing color and screen
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //drawing the game
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
