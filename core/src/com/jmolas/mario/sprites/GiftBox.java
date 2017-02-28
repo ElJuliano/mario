bb2a8a992eef3e07df78f84482a6107593479a54
Julien <molasjulien@gmail.com>
2017-02-28 22:56:42 +0100

    --Part 14 -- Adding gift box change on hit

--- core/src/com/jmolas/mario/sprites/GiftBox.java
+++ core/src/com/jmolas/mario/sprites/GiftBox.java
@@ -1,13 +1,13 @@
 package com.jmolas.mario.sprites;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.maps.Map;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.jmolas.mario.MarioBros;
+import com.jmolas.mario.scenes.Hud;
 
 /**
  * Created by Julien on 06/02/2017.
@@ -15,8 +15,12 @@ import com.jmolas.mario.MarioBros;
 
 public class GiftBox extends InteractiveTileObject {
 
+    private static TiledMapTileSet tileSet;
+    private final int BLANK_GIFT = 28;
+
     public GiftBox(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
+        tileSet = map.getTileSets().getTileSet("Mario");
         fixture.setUserData(this);
         setCategoryFilter(MarioBros.GIFT_BIT);
     }
@@ -24,6 +28,7 @@ public class GiftBox extends InteractiveTileObject {
     @Override
     public void onHeadHit() {
         Gdx.app.log("gift.collision", "");
+        getCell().setTile(tileSet.getTile(BLANK_GIFT));
+        Hud.addScore(100);
     }
-
 }
